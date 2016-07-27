cc62092774a81dd0e1aba3278f61144f63465ee6
szaim <zaimsofia81@gmail.com>
2016-07-27 14:43:21 -0400

    final work

--- app.js
+++ app.js
@@ -1,43 +1,59 @@
-var API_KEY = 'AIzaSyCry9T_zQjX12X0wx6Dcmr10taBW4Y9_Ps';
+
 
 $(document).ready(function() {
 
-function displayData(data) {
-		console.log(data);
-}
 
-// var queryTerm = 'cats';
-// var searchData {
-// 	q: queryTerm,
+	function getResults(data) {
+		$.getJSON("https://www.googleapis.com/youtube/v3/search",
+			{
+				part: "snippet",
+				key: "AIzaSyCry9T_zQjX12X0wx6Dcmr10taBW4Y9_Ps",
+				q: data
+			},
+				function (data) {
+			
+				displaySearchResults(data.items);
+			}
+
+		);
+	}
+
+	function displaySearchResults(videos) {
+		var html = "";
+		$.each(videos, function (index, video) {
+			// append li to ul
+
+			console.log(video);
+			console.log(video.id.videoId);
+			html = html + "<li><p>" + video.snippet.title +
+				"</p><a href='https://www.youtube.com/watch?v=''"+video.id.videoId+ "'target='_blank'>"
+					+"<img src='" +  video.snippet.thumbnails.high.url + "'/></a></li>" ;
+
+		});
+		$("#content").html(html);
+	}
+
+	$("form").submit(function (event) {
+		event.preventDefault();
+		getResults($("#search-item").val());
+	});
 
 
+});
+
+
+
+// / Create a listing for a video.
+// function displayResult(videoSnippet) {
+//   var title = videoSnippet.title;
+//   var videoId = videoSnippet.resourceId.videoId;
+//   $('#video-container').append('<p>' + title + ' - ' + videoId + '</p>');
 // }
 
-// "https://www.googleapis.com/youtube/v3/search?q=cats&eyk='
-
-$.ajax({
-  method: "GET",
-  dataType: "json",
-  url:"https://www.googleapis.com/youtube/v3/search?q=cats&key=AIzaSyCry9T_zQjX12X0wx6Dcmr10taBW4Y9_Ps",
-  success: displayData,
-  //data: 
-});
-
-
-
-
-//https://www.googleapis.com/youtube/v3/search
-
-
-
-//AIzaSyCry9T_zQjX12X0wx6Dcmr10taBW4Y9_Ps
 
 
 
 
 
-
-
-});
 
 
